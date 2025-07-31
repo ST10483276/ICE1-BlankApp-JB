09e9ff6bfc16cbedd61a628c4314eca8e11b8415
Jason Barasa <ST10483276@vcconnect.edu.za>
2025-07-31 19:30:22 +0200

    Initial commit

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.greetings}>Hello Jason!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#C6F1F1',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  greetings: {
+    fontSize: 28,
+    fontWeight: 'bold',
   },
 });
